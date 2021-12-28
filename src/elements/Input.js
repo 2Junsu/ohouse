59246ebe52ba3107d68506cf60d949c1e9001b70
2Junsu <ijh1205@naver.com>
2021-12-29 04:34:41 +0900

    input box border color 지정 가능하도록 수정

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -3,12 +3,15 @@ import styled from 'styled-components'
 import { Text } from './index'
 
 const Input = (props) => {
-  const { width, placeholder, onChange } = props
+  const { width, placeholder, onChange, borderColor } = props
   return (
     <Fragment>
       <_Input
         {...props}
-        style={{ fontSize: '15px', fontWeight: '500' }}
+        style={{
+          fontSize: '15px',
+          fontWeight: '500',
+        }}
         placeholder={placeholder}
         onChange={onChange}
       />
@@ -19,13 +22,14 @@ const Input = (props) => {
 Input.defaultProps = {
   placeholder: '',
   width: '100%',
+  borderColor: '#cccccc',
   onChange: () => {},
 }
 
 const _Input = styled.input`
   width: ${(props) => props.width};
   padding: 14px;
-  border: 1px solid #cccccc;
+  border: 1px solid ${(props) => props.borderColor};
   border-radius: 8px;
   box-sizing: border-box;
 `
